45b3703cf6d5bb02dbc0016195d57da64b050d4b
onakaponpon <sano@okosama.org>
2019-12-03 10:19:42 +0900

    少し修正

--- script/makeKananeruLinkQR.js
+++ script/makeKananeruLinkQR.js
@@ -1,30 +1,5 @@
-/*
-https://disaportal.gsi.go.jp/maps/index.html?
-ll=35.125665,136.738844&z=13
-&base=pale&ls=seamless%7Ctameike_raster%2C0.8%7Cflood_list%2C0.8%7Cflood_list_l2%2C0.8%7Cdisaster1&disp=01010&vs=c1j0l0u0
-*/
-
-// function makeKasaneruMapQR(x,y,z){
-//   var returnString = "https://disaportal.gsi.go.jp/maps/index.html?ll="+x+","+y+"&z="+z
-//   +"&base=pale&ls=seamless%7Ctameike_raster%2C0.8%7Cflood_list%2C0.8%7Cflood_list_l2%2C0.8%7Cdisaster1&disp=01010&vs=c1j0l0u0";
-//   return returnString;
-// }
-
-// function makeKasaneruMapQR(x,y,z){
-//   var returnString = "https://disaportal.gsi.go.jp/maps/index.html?ll="+x+","+y+"&z="+z
-//   +"&base=pale&ls=seamless%7Ctameike_raster%2C0.8%7Cflood_list%2C0.8%7Cflood_list_l2%2C0.8%7Cdisaster1&disp=01010&vs=c1j0l0u0";
-//     $('#qrcode').qrcode({
-//         text: returnString,
-//         width: 200,
-//         height: 200,
-//         background : "#fff",
-//         foreground : "#000"
-//     });
-// }
-
-https://disaportal.gsi.go.jp/maps/index.html?ll=35.269764,138.435974&z=14&base=pale&ls=seamless%7Ctameike_raster%2C0.8%7Cflood_list%2C0.8%7Cflood_list_l2%2C0.8%7Cdosha_kiken_nadare%2C0.8%7Cdosha_kiken_jisuberi%2C0.8%7Cdosha_kiken_kyukeisha%2C0.8%7Cdosha_kiken_dosekiryu%2C0.8%7Cdosha_keikai_jisuberi%2C0.8%7Cdosha_keikai_dosekiryu%2C0.8%7Cdosha_keikai_kyukeisha%2C0.8%7Cdisaster1%7Cdisaster2&disp=0111111111111&vs=c1j0l0u0
 $(function makeKasaneruMapQR(x,y,z){
-	//現在表示しているページのURLをQRコード化する
+	//座標を渡した文字列
     var returnString = "https://disaportal.gsi.go.jp/maps/index.html?ll="+x+","+y+"&z="+z
     +"&base=pale&ls=seamless%7Ctameike_raster%2C0.8%7Cflood_list%2C0.8%7Cflood_list_l2%2C0.8%7Cdisaster1&disp=01010&vs=c1j0l0u0";
 
